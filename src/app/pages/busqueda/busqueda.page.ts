a0f36d85b42171b5d9ad73f4b10363886930b4d0
LaraHV17 <lhv17gj@gmail.com>
2024-09-12 20:50:03 +0200

    bsuqueda1

--- src/app/pages/busqueda/busqueda.page.ts
+++ src/app/pages/busqueda/busqueda.page.ts
@@ -1,30 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
 import { PicturesService } from 'src/app/services/http-nasa/http-nasa.service';
+import { Apod } from 'src/app/interfaces/apod';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.page.html',
   styleUrls: ['./busqueda.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class BusquedaPage implements OnInit {
+  public pictures: Apod[] = [];
   public dateBuscada: string = "";
+  public cargando: boolean = false;
+  constructor(private picturesService: PicturesService) { }
 
-  constructor() { private picturesService: PicturesService
-} {
+  ngOnInit() {
+  }
+
+  buscar() {
+    this.cargando = true;
+    this.picturesService.getPictures(this.dateBuscada).subscribe(data => {
+      this.cargando = false;
+      if (data.Response === 'False') { }
+      else {
+        this.pictures = data.Search;
+      }
+    })
+  }
 
 }
 
-ngOnInit() {
-}
 
-buscar() {
-  this.cargando
-  this.picturesService.getPictures(this.dateBuscada)
-}
 
-}
