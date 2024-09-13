65e6b6e389b2776a6b3b907c46ea604200acf53a
LaraHV17 <lhv17gj@gmail.com>
2024-09-13 20:26:47 +0200

    h

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutlin
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
+    { title: 'Búsqueda', url: '/folder/busqueda', icon: 'search' },
     { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
     { title: 'Archived', url: '/folder/archived', icon: 'archive' },
--- src/app/services/http-nasa/http-nasa.service.ts
+++ src/app/services/http-nasa/http-nasa.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { delay, Observable } from 'rxjs';
 
 const BASE_URL = "https://api.nasa.gov/planetary/apod";
 const API_KEY = "YVeUfTZHFpIQkT5bMClwDd5JSDRfSyBl0I8HWKLP";
@@ -12,11 +12,15 @@ export class PicturesService {
   private http = inject(HttpClient);
   public pictures: any[] = [];
   constructor() { }
+
+  getPictures(date: string): Observable<any> {
+    return this.http
+      .get<any>(`${BASE_URL}?apikey=${API_KEY}&date=${date}`)
+      .pipe(delay(1000));
+  }
+
+
+
 }
 
 //Para buscar todas las fotos
-getPictures(date: string): Observable < any > {
-  return this.http
-    .get<any>(`${BASE_URL}?apikey=${API_KEY}&date=${date}`)
-    .pipe(delay(1000));
-}
